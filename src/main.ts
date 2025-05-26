dfa2ae49ef43232e0408a009afde8eafeb221f04
JeanV1157 <jeanfrank_1157@hotmail.com>
2025-05-25 21:16:42 -0500

    Prueba2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-//comentario
+//Hello
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const server = app.getHttpServer();
